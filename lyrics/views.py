c1c0aabe4a347f5e1642f5b5d0b5b751ac0947e8
Samarpan Mathangi <samarpanraaz@gmail.com>
2024-05-25 23:21:12 +0000

    create listview with filter

--- lyrics/views.py
+++ lyrics/views.py
@@ -6,5 +6,6 @@ from .models import Lyric
 # Create your views here.
 
 class LyricList(generic.ListView):
-     queryset = Lyric.objects.all()
+     queryset = Lyric.objects.filter(status=1)
      template_name = "lyric_list.html"
+
