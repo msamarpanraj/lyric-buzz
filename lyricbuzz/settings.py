8f4c2cc3e31b9375c43202325fb71cb867aca431
Samarpan Mathangi <samarpanraaz@gmail.com>
2024-05-25 19:28:17 +0000

    add lyric model

--- lyricbuzz/settings.py
+++ lyricbuzz/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-msamarpanraj-lyricbuzz-wguyhk6h9ky.ws.codeinstitute-ide.net','.herokuapp.com'
 ]
@@ -131,3 +131,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+    "https://*.codeinstitute-ide.net"
+]
--- lyrics/models.py
+++ lyrics/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Lyric(models.Model):
+    song_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    lyric_writer = models.CharField(max_length=100)
+    album = models.CharField(max_length=100)
+    lyrics = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='added_lyrics')
