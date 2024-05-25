6d157962acf3714b0048feaca6b92a648b1c2619
Samarpan Mathangi <samarpanraaz@gmail.com>
2024-05-25 18:44:40 +0000

    update secret key

--- lyricbuzz/settings.py
+++ lyricbuzz/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ne66)uv6tby^!*%i(tktt9d&8t5epc-wt9o(_dku+i#5ll^x$v'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
