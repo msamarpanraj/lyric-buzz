26292fc69a97f12af8173cd7b6fca7dfba171f69
Samarpan Mathangi <samarpanraaz@gmail.com>
2024-05-25 19:53:14 +0000

    add updated on field to lyric model

--- lyrics/models.py
+++ lyrics/models.py
@@ -11,5 +11,6 @@ class Lyric(models.Model):
     album = models.CharField(max_length=100)
     lyrics = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='added_lyrics')
