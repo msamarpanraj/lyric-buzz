2c7f04ed41f346326343ed7905bec968744273d9
Samarpan Mathangi <samarpanraaz@gmail.com>
2024-05-25 21:58:05 +0000

    add meta options to comment model

--- lyrics/models.py
+++ lyrics/models.py
@@ -30,3 +30,8 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.comment_text} by {self.user}"
