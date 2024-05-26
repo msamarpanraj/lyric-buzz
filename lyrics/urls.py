5463f2e75bf9576550b6ac7f35c3bdc00229f7b5
Samarpan Mathangi <samarpanraaz@gmail.com>
2024-05-26 03:27:29 +0000

    added lyric detail view

--- lyrics/urls.py
+++ lyrics/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.LyricList.as_view(), name='home'),
+    path('<slug:slug>/', views.lyric_detail, name='lyric_detail'),
+
 ]
--- lyrics/views.py
+++ lyrics/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Lyric
 
@@ -11,4 +11,25 @@ class LyricList(generic.ListView):
      paginate_by = 6
 
 
+def lyric_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
 
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Lyric.objects.filter(status=1)
+    lyric = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "lyrics/lyric_detail.html",
+        {"lyric": lyric},
+    )
