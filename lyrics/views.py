a57f0eca06e95e2074eb5aa509a20dabcab46f0b
Samarpan Mathangi <samarpanraaz@gmail.com>
2024-05-27 00:47:44 +0000

    setup comment forms

--- lyrics/views.py
+++ lyrics/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Lyric
+from .forms import CommentForm
 
 
 # Create your views here.
@@ -29,6 +31,21 @@ def lyric_detail(request, slug):
     lyric = get_object_or_404(queryset, slug=slug)
     comments = lyric.comments.all().order_by("-created_on")
     comment_count = lyric.comments.filter(approved=True).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if  comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.user = request.user
+            comment.lyric = lyric
+            comment.save()
+            messages.add_message(
+        request, messages.SUCCESS,
+        'Comment submitted and awaiting approval'
+    )
+
+    comment_form = CommentForm()
+
     
     return render(
     request,
@@ -37,5 +54,7 @@ def lyric_detail(request, slug):
         "lyric": lyric,
         "comments": comments,
         "comment_count": comment_count,
+        "comment_form": comment_form,
+
     },
 )
