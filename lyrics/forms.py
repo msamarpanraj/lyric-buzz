4b510e9870f512d10b4a26519dc0c768fc25ff75
Samarpan Mathangi <samarpanraaz@gmail.com>
2024-05-31 16:18:29 +0000

    update submit form fields

--- lyrics/forms.py
+++ lyrics/forms.py
@@ -4,31 +4,42 @@ from django_summernote.widgets import SummernoteWidget
 from django.contrib.auth.models import User
 
 
-
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('comment_text',)
+        fields = ("comment_text",)
+
 
 class LyricSearchForm(forms.Form):
-    q = forms.CharField(label='Search for lyrics', max_length=100)
+    q = forms.CharField(label="Search for lyrics", max_length=100)
 
 
 class LyricSubmissionForm(forms.ModelForm):
     class Meta:
         model = Lyric
-        fields = ['song_name', 'slug', 'lyric_writer', 'album', 'featured_image', 'lyrics']
+        fields = [
+            "song_name",
+            "slug",
+            "lyric_writer",
+            "album",
+            "featured_image",
+            "lyrics",
+        ]
         widgets = {
-            'lyrics': SummernoteWidget(),
+            "lyrics": SummernoteWidget(),
         }
 
+
 class ProfileForm(forms.ModelForm):
-    profile_image = forms.ImageField(label='Profile Image', required=False, widget=forms.FileInput)
+    profile_image = forms.ImageField(
+        label="Profile Image", required=False, widget=forms.FileInput
+    )
 
     class Meta:
         model = Profile
-        fields = ['profile_image', 'bio']
+        fields = ["profile_image", "bio"]
         widgets = {
-            'bio': forms.Textarea(attrs={'maxlength': '500'}),  # assuming you want a 500 character limit
+            "bio": forms.Textarea(
+                attrs={"maxlength": "500"}
+            ), 
         }
-        
